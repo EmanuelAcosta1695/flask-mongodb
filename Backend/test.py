9f4cf773ef3666306e427c5c77ac39bc6ef8f53f
Emanuel Acosta <e.acosta1695@gmail.com>
2023-07-01 15:11:41 -0300

    update file test.py

--- Backend/test.py
+++ Backend/test.py
@@ -2,14 +2,12 @@ import unittest
 from app import app
 from flask_pymongo import PyMongo
 
+# Crea y elimina el user
 class AppTest(unittest.TestCase):
-
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
         self.mongo = PyMongo(app)
-        self.user_id = None  # Variable para almacenar el ID del usuario creado
-
 
     def test_create_user(self):
         response = self.app.post('/users', data={
@@ -19,26 +17,63 @@ class AppTest(unittest.TestCase):
         })
         self.assertEqual(response.status_code, 200)
         data = response.get_json()
-        self.user_id = data['id']  # Almacenar el ID del usuario creado
+        print(data)
+
+
+    def test_get_users(self):
+        response = self.app.get('/mostrarUsers/')
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+       
+        if data:
+            user_id = str(data[-1]['_id']['$oid'])
+            print('test_get_users', user_id)
+            return user_id
+        
+        else:
+            self.skipTest("No users available")
 
 
     def test_get_user(self):
-        response = self.app.get(f'/users/{self.user_id}')  # Usar el ID almacenado
-        self.assertEqual(response.status_code, 200)
+        user_id = self.test_get_users()
+        print('test_get_user', user_id)
+       
+        if user_id is not None:
+            response = self.app.get(f'/users/{user_id}')
+            self.assertEqual(response.status_code, 200)
+        
+        else:
+            self.skipTest("No user_id available")
 
 
     def test_update_user(self):
-        response = self.app.put(f'/users/{self.user_id}', json={
-            'username': 'John Doe',
-            'email': 'johndoe@example.com'
-        })
-        self.assertEqual(response.status_code, 200)
+        user_id = self.test_get_users()
+        print('test_update_user', user_id)
+
+        if user_id is not None:
+            response = self.app.put(f'/users/{user_id}', json={
+                'username': 'John Doe',
+                'email': 'johndoe@example.com'
+            })
+            self.assertEqual(response.status_code, 200)
+
+        else:
+            self.skipTest("No user_id available")
 
 
     def test_delete_user(self):
-        response = self.app.delete(f'/users/{self.user_id}')
-        self.assertEqual(response.status_code, 200)
+        user_id = self.test_get_users()
+        print('test_delete_user', user_id)
+        if user_id is not None:
+            response = self.app.delete(f'/users/{user_id}')
+            self.assertEqual(response.status_code, 200)
+        else:
+            self.skipTest("No user_id available")
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
